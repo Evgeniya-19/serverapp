e098a89bf0b11c464b861a157c1cb8b336638cda
E <jeekaaa@mail.ru>
2022-02-01 08:53:55 +0300

    hw_2.5

--- client.py
+++ client.py
@@ -1,87 +1,83 @@
-"""Программа-клиент"""
-
-import sys
-import json
-import socket
-import time
-import argparse
 import logging
 import logs.config_client_log
-from errors import ReqFieldMissingError
-from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE, DEFAULT_PORT, ERROR, DEFAULT_IP_ADDRESS
-from common.utils import get_message, send_message
+import argparse
+import sys
+from PyQt5.QtWidgets import QApplication
 
+from common.variables import *
+from common.errors import ServerError
+from common.decos import log
+from client.database import ClientDatabase
+from client.transport import ClientTransport
+from client.main_window import ClientMainWindow
+from client.start_dialog import UserNameDialog
 
 # Инициализация клиентского логера
-CLIENT_LOGGER = logging.getLogger('client')
+logger = logging.getLogger('client')
 
 
-def create_presence(account_name='Guest'):
-    out = {
-        ACTION: PRESENCE,
-        TIME: time.time(),
-        USER: {
-            ACCOUNT_NAME: account_name
-        }
-    }
-    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
-    return out
-
-
-def process_ans(message):
-
-    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
-    if RESPONSE in message:
-        if message[RESPONSE] == 200:
-            return '200 : OK'
-        return f'400 : {message[ERROR]}'
-    raise ReqFieldMissingError(RESPONSE)
-
-
-def create_arg_parser():
-
+# Парсер аргументов коммандной строки
+@log
+def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
     parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
-    return parser
-
-
-def main():
-
-    parser = create_arg_parser()
+    parser.add_argument('-n', '--name', default=None, nargs='?')
     namespace = parser.parse_args(sys.argv[1:])
     server_address = namespace.addr
     server_port = namespace.port
+    client_name = namespace.name
 
     # проверим подходящий номер порта
     if not 1023 < server_port < 65536:
-        CLIENT_LOGGER.critical(
-            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
-            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
-        sys.exit(1)
+        logger.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}. Допустимы адреса с 1024 до 65535. Клиент завершается.')
+        exit(1)
 
-    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
-                       f'адрес сервера: {server_address} , порт: {server_port}')
-    # Инициализация сокета и обмен
-
-    try:
-        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        transport.connect((server_address, server_port))
-        message_to_server = create_presence()
-        send_message(transport, message_to_server)
-        answer = process_ans(get_message(transport))
-        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
-        print(answer)
-    except json.JSONDecodeError:
-        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
-    except ConnectionRefusedError:
-        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
-                               f'конечный компьютер отверг запрос на подключение.')
-    except ReqFieldMissingError as missing_error:
-        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
-                            f'{missing_error.missing_field}')
+    return server_address, server_port, client_name
 
 
+# Основная функция клиента
 if __name__ == '__main__':
-    main()
+    # Загружаем параметы коммандной строки
+    server_address, server_port, client_name = arg_parser()
+
+    # Создаём клиентокое приложение
+    client_app = QApplication(sys.argv)
+
+    # Если имя пользователя не было указано в командной строке то запросим его
+    if not client_name:
+        start_dialog = UserNameDialog()
+        client_app.exec_()
+        # Если пользователь ввёл имя и нажал ОК, то сохраняем ведённое и удаляем объект, инааче выходим
+        if start_dialog.ok_pressed:
+            client_name = start_dialog.client_name.text()
+            del start_dialog
+        else:
+            exit(0)
+
+    # Записываем логи
+    logger.info(
+        f'Запущен клиент с парамертами: адрес сервера: {server_address} , порт: {server_port}, имя пользователя: {client_name}')
+
+    # Создаём объект базы данных
+    database = ClientDatabase(client_name)
+
+    # Создаём объект - транспорт и запускаем транспортный поток
+    try:
+        transport = ClientTransport(server_port, server_address, database, client_name)
+    except ServerError as error:
+        print(error.text)
+        exit(1)
+    transport.setDaemon(True)
+    transport.start()
+
+    # Создаём GUI
+    main_window = ClientMainWindow(database, transport)
+    main_window.make_connection(transport)
+    main_window.setWindowTitle(f'Чат Программа alpha release - {client_name}')
+    client_app.exec_()
+
+    # Раз графическая оболочка закрылась, закрываем транспорт
+    transport.transport_shutdown()
+    transport.join()
